eacdbfb344c51a56a08ba7ba8ad823ab432d06cc
Yoko.Okamoto <ec2-user@ip-172-31-40-144.us-east-2.compute.internal>
2018-05-07 13:42:19 +0000

    microposts

---
+++ db/migrate/20180414140658_create_favolites.rb
@@ -0,0 +1,12 @@
+class CreateFavolites < ActiveRecord::Migration[5.0]
+  def change
+    create_table :favolites do |t|
+      t.references :user, foreign_key: true
+      t.references :micropost, foreign_key: { to_table: :users }
+
+      t.timestamps
+      
+      t.index [:user_id, :micropost_id], unique: true
+    end
+  end
+end
